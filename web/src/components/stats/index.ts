3f481702b87199eb89fc2f908372fa4d53133094
NektariosTP <nektar.tp@gmail.com>
2026-09-09 18:58:10 +0300

    feat(scope): delete stats/context/region UI + dead coming-soon stubs

--- web/src/components/stats/index.ts
+++
@@ -1,5 +0,0 @@
-export { KpiStrip } from "./KpiStrip";
-export { ActivityChart } from "./ActivityChart";
-export { AxisBreakdownBar } from "./AxisBreakdownBar";
-export { TopLocationsTable } from "./TopLocationsTable";
-export { TopClustersTable } from "./TopClustersTable";
