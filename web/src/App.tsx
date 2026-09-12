fb6432f07b62e0171f9844a9af7977f283b5053c
NektariosTP <nektar.tp@gmail.com>
2026-09-12 23:39:44 +0300

    feat(web): remove temporary mobile coming-soon banner

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MainView } from "./pages/MainView";
-import { MobileNotice } from "./components/layout";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 30_000, refetchOnWindowFocus: false } },
@@ -11,7 +10,6 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <MobileNotice />
         <Routes>
           <Route path="/" element={<MainView />} />
           <Route path="/cluster/:id" element={<MainView />} />
