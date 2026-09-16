142dabf97e2e803170443e4043bebc361ce6a383
NektariosTP <nektar.tp@gmail.com>
2026-09-16 03:29:14 +0300

    refactor(web): drop dead TemporalBlock, temporal queries and i18n

--- web/src/components/shell/index.ts
+++ web/src/components/shell/index.ts
@@ -1,6 +1,5 @@
 export { HeaderBlock } from "./HeaderBlock";
 export { EditorialBlock } from "./EditorialBlock";
-export { TemporalBlock } from "./TemporalBlock";
 export { UserControls } from "./UserControls";
 export { BottomSheet } from "./BottomSheet";
 export { LegendPanel } from "./LegendPanel";
